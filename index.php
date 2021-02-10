1ff8257f7ffb5b4a1961a47a38d8ec473aafe03e
Maxim-X <43073464+Maxim-X@users.noreply.github.com>
2021-02-10 14:39:59 +0500

    Start

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Лендинг DROID</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+</head>
+<body>
+	<header>
+		<div class="head">
+			<div class="top_head">
+				<div class="logo">
+					<div class="img"><img src="/image/logo.svg" alt="logo"></div>
+					<div class="head_comp">DROID</div>
+				</div>
+				<div class="dop_info">
+					<div class="time">Ежедневно, 10:00 - 20:00</div>
+					<div>8(3822)94-06-06</div>
+					<div>service@2droida.ru</div>
+				</div>
+			</div>
+			<div class="bottom_head">
+				<nav>
+					<a href="#"><li>О нас</li></a>
+					<a href="#"><li>Цены</li></a>
+					<a href="#"><li>Контакты</li></a>
+					<a href="#"><li>Вакансии</li></a>
+				</nav>
+
+				<div class="soc">
+					<div class="soc_url"><a href="https://vk.com/droid" target="_blank">Вконтакте</a></div>
+					<div class="soc_url"><a href="https://instagram.com/droid" target="_blank">Instagram</a></div>
+					<div class="soc_url"><a href="whatsapp://send?phone=7903950645" target="_blank">WhatsApp</a></div>
+				</div>
+			</div>
+		</div>
+	</header>
+	<section id="all_def">
+		<div class="container">
+			<div class="row">
+				<div class="col-8">
+					<div class="all_el_def">
+						<div class="el_def">
+							<div class="image"></div>
+							<h2>Замена дисплейного<br> модуля</h2>
+							<p>Разбит дисплей, не работает сенсор, не показывает экран</p>
+							<div class="price">от 1000</div>
+						</div>
+						<div class="el_def">
+							<div class="image"></div>
+							<h2>Замена<br> аккумулятора</h2>
+							<p>Не держит заряд, выключается на холоде или вздулся аккумулятор. А так же, если попала влага или был сильный удар.</p>
+							<div class="price">от 4000</div>
+						</div>
+						<div class="el_def">
+							<div class="image"></div>
+							<h2>Обновление программного обеспечения</h2>
+							<p>Ошибки приложений Android, не работает Google Play, не открывается галерея, Не видит сеть, нет русского языка.</p>
+							<div class="price">от 300</div>
+						</div>
+						<div class="el_def">
+							<div class="image"></div>
+							<h2>Чистка от коррозии после попадания жидкости</h2>
+							<p>Залит телефон, внутри влага, утопили.</p>
+							<div class="price">от 1000</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-4">
+					<div class="main_form">
+						<div class="form">
+							<h2>Обратный звонок</h2>
+							<form>
+								<div class="mb-1">
+									<label for="name">Ваше имя:</label>
+									<input type="text" name="name" placeholder="Введите ваше имя">
+								</div>
+								<div class="mb-1">
+									<label for="phone">Ваш телефон:</label>
+									<input type="text" name="phone" placeholder="Введите ваш телефон">
+								</div>
+								<div class="mb-1">
+									<input type="submit" name="send" value="Позвонить мне">
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</body>
+</html>
